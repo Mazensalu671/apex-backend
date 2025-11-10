3e1a3dc6b6b0c55250e17f5f490d14029f791345
mazen saleh <mazensalu671@gmail.com>
2025-11-10 12:44:17 +0300

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 10000),
+  url: env('PUBLIC_URL', 'https://apex-backend-i4jc.onrender.com'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['randomAppKey123', 'anotherKey456']),
   },
 });
