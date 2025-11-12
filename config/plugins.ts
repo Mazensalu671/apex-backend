fbe4694427190da12c208b38a4c53115eb25b15e
mazen saleh <mazensalu671@gmail.com>
2025-11-12 10:15:39 +0300

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   upload: {
     config: {
       // ✅ لاحظ .ts في النهاية
-      provider: './config/upload-cloudflare.ts',
+      provider: './config/upload-cloudflare',
       providerOptions: {},
     },
   },
