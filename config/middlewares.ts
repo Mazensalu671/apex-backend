0d0739b7078017711738e41518b1490f3445e4e8
mazen saleh <mazensalu671@gmail.com>
2025-11-13 13:11:30 +0300

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,32 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://market-assets.strapi.io",
+            "https://ik.imagekit.io/uwhb4abom",
+            "*.imagekit.io"
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://ik.imagekit.io/uwhb4abom",
+            "*.imagekit.io"
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
-  'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
