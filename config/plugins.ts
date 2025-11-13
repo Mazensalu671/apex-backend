b28777c8c5fb0136cd5416923f9d3ff3eb3be32e
mazen saleh <mazensalu671@gmail.com>
2025-11-13 11:30:41 +0300

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-imagekit',
+      providerOptions: {
+        publicKey: env('IMAGEKIT_PUBLIC_KEY'),
+        privateKey: env('IMAGEKIT_PRIVATE_KEY'),
+        urlEndpoint: env('IMAGEKIT_URL_ENDPOINT'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
 
