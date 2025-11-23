dfc080f9439e6ac4b5100516f6cb204c6e73bd71
mazen saleh <mazensalu671@gmail.com>
2025-11-23 14:05:30 +0300

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,11 @@
-module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'https://apex-backend-production-a86e.up.railway.app'),
+export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
+  // IMPORTANT — DO NOT USE OLD RENDER URL
+  url: env('PUBLIC_URL'),
+
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
